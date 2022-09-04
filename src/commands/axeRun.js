6009fba9438619d5c332a91e7a31ccf11db15be8
Ravi Sawlani <ravisawlani04@gmail.com>
2022-09-04 11:58:15 +0530

    refactor

--- src/commands/axeRun.js
+++ src/commands/axeRun.js
@@ -1,53 +1,45 @@
-module.exports.command = function axeRun(selector = 'html', options = {}) {
-  this.executeAsync(function (selector, options, done) {
-
-      (function (axe) {
-          if (!axe) done({
-              error: 'aXe not found. Make sure it has been injected'
-          })
-          var el = document.querySelector(selector)
+const Events = require('events');
+module.exports = class AxeRun {
+  async command(selector = 'html', options = {}) {
+    const value = await this.api.executeAsync(
+      function (selector, options, done) {
+        (function (axe) {
+          if (!axe)
+            done(new Error('aXe not found. Make sure it has been injected'));
+          var el = document.querySelector(selector);
 
           axe.run(el, options, function (err, results) {
-              if (err) {
-                  done({
-                      error: err.toString()
-                  })
-              }
-              done({
-                  results: results
-              })
-          })
-      })(window.axe)
+            if (err) {
+              done(err);
+            }
+            done({
+              results: results
+            });
+          });
+        })(window.axe);
+      },
+      [selector, options]
+    );
+    const { results } = value;
 
-  }, [selector, options], function (response) {
-      const {
-          error,
-          results
-      } = response.value
+    const { passes = [], violations = [] } = results;
 
-      const {
-          passes = [], violations = []
-      } = results
-
-      for (let i = 0; i < passes.length; i++) {
-          this.assert.ok(true, `aXe rule: ${passes[i].id} (${passes[i].nodes.length} elements checked)`);
+    const Asserts = passes.map((pass) =>
+      this.api.assert.ok(
+        true,
+        `aXe rule: ${pass.id} (${pass.nodes.length} elements checked)`
+      )
+    );
+    const failures = violations.map((violate) => {
+      let nodeName = violate.nodes[n].target.toString();
+      if (nodeName.length > 100) {
+        nodeName = '...' + nodeName.slice(-100);
       }
 
-      for (let i = 0; i < violations.length; i++) {
+      let assertionFailureMessage = `aXe rule: ${violate.id} - ${violate.help}\r\n\tIn element: ${nodeName}`;
+      return this.api.verify.fail(assertionFailureMessage);
+    });
 
-          for (let n = 0; n < violations[i].nodes.length; n++) {
-
-              let nodeName = violations[i].nodes[n].target.toString();
-              if (nodeName.length > 100) {
-                  nodeName = "..." + nodeName.slice(-100);
-              }
-
-              let assertionFailureMessage =
-                  `aXe rule: ${violations[i].id} - ${violations[i].help}\r\n\tIn element: ${nodeName}`;
-              this.verify.fail(assertionFailureMessage);
-          }
-      }
-  })
-
-  return this
-}
+    return await Promise.all([...Asserts, ...failures]);
+  }
+};
