b210a0f8cc10d8e792c08b2e68425016c13e1894
Sally Young <git@justa.fish>
2023-07-05 13:34:10 +0100

    Fix path resolution for axe.min.js

--- nightwatch/commands/axeInject.js
+++ nightwatch/commands/axeInject.js
@@ -1,35 +1,17 @@
 const fs = require('fs');
 const path = require('path');
 
-const localPath = path.join(
-  __dirname,
-  '..',
-  '..',
-  'node_modules',
-  'axe-core',
-  'axe.min.js'
-);
-const parentPath = path.join(
-  __dirname,
-  '..',
-  '..',
-  '..',
-  'axe-core',
-  'axe.min.js'
-);
-
-const axe = fs.existsSync(localPath)
-  ? fs.readFileSync(localPath, 'utf8')
-  : fs.readFileSync(parentPath, 'utf8');
+const axePath = require.resolve('axe-core/axe.min.js');
+const axe = fs.readFileSync(axePath, 'utf8');
 
 module.exports = class AxeInject {
   command() {
     return this.api.execute(
-      (js) => {
-        // eslint-disable-next-line no-eval
-        eval(js);
-      },
-      [axe]
+        (js) => {
+          // eslint-disable-next-line no-eval
+          eval(js);
+        },
+        [axe]
     );
   }
 };
