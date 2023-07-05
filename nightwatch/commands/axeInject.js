e2ccac19351b58c2ab42435e86ff66437ba682e1
Sally Young <git@justa.fish>
2023-07-05 13:36:36 +0100

    Fix spacing

--- nightwatch/commands/axeInject.js
+++ nightwatch/commands/axeInject.js
@@ -7,11 +7,11 @@ const axe = fs.readFileSync(axePath, 'utf8');
 module.exports = class AxeInject {
   command() {
     return this.api.execute(
-        (js) => {
-          // eslint-disable-next-line no-eval
-          eval(js);
-        },
-        [axe]
+      (js) => {
+        // eslint-disable-next-line no-eval
+        eval(js);
+      },
+      [axe]
     );
   }
 };
